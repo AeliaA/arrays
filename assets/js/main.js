f919ab3417d6821059b7b633506bc922783aa200
Aelia Abbas <aelia.online@gmail.com>
2015-11-16 18:57:01 +1300

    1

---
+++ assets/js/main.js
@@ -0,0 +1,88 @@
+// var myShop = ['bread', '2', 'true'];
+
+// var myShop = ['bread', 'milk', 'cheese'];
+
+// var myShop = [
+// 		'bread',
+// 		'milk',
+// 		'cheese'
+// 		];
+
+// alert( myShop[1]);
+
+// console.log(myShop[0]);
+
+// this code is adding another item 'egg' to the array
+
+// myShop [myShop.length] = 'egg';
+
+// other way of adding item to an array is:
+
+// myShop.push(7, 8, 9);
+
+// alert( myShop[4]);
+
+// other way of adding item to an array, but not the end, at the front is:
+
+// myShop.unshift(1, 2, 3);
+
+// alert( myShop[0]);
+
+
+
+// create a var with an empty array
+
+var colorList = [];
+
+//Add one item to end of colorList using push method
+
+colorList.push('red');
+
+// alert (colorList[0]);
+
+// To see what the push method did add a printList function
+
+// printList( colorList );
+
+// adding 2 items to end of list
+
+colorList.push('green', 'blue');
+
+// adding 1 item to front of list
+
+colorList.unshift('orange');
+
+// adding 2 items to front of list
+
+colorList.unshift('purple', 'pink');
+
+//printList must be used once at the end to get the result
+
+printList( colorList );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
